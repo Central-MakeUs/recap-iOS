53e73760719eba962f4a909d5d089f013deac092
Hong Seokho <hongseokhodev@gmail.com>
2026-06-29 22:22:21 +0900

    Initial Commit

---
+++ Recap-demo/Recap_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recap_demoApp.swift
+//  Recap-demo
+//
+//  Created by oliver on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Recap_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
